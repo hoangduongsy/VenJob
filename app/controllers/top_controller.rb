cfd5dba8ad8b7af147b6d69d7c1eed73d7abb2b8
ĐƯờng Sỹ Hoàng <hoangds@zigexn.vn>
2019-12-18 15:38:10 +0700

    Changed setting

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,6 +1,6 @@
 class TopController < ApplicationController
   def index
-    @latest_jobs = Job.latest_jobs.take(5)
-    @top_cities = City.sort_top_cities.take(9)
+    @latest_jobs = Job.latest_jobs.take(Settings.top.job.limit)
+    @top_cities = City.sort_top_cities.take(Settings.top.city.limit)
   end
 end
