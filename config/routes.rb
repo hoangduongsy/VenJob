a861c7418a6c93c56a315cd18fd4c2ef3051b514
ĐƯờng Sỹ Hoàng <hoangds@zigexn.vn>
2019-12-25 13:35:18 +0700

    Removed files

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   root "top#index"
   resources :cities, only: :index
   resources :industries, only: :index
-  resources :login, only: :index
   resource :top, only: :index
   devise_for :users
 end
