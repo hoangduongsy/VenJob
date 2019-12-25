9f03ade73924e9ee65f143960b6ab746a59c9ad7
ĐƯờng Sỹ Hoàng <hoangds@zigexn.vn>
2019-12-25 13:29:16 +0700

    Removed files

--- app/models/city.rb
+++ app/models/city.rb
@@ -3,7 +3,7 @@ class City < ApplicationRecord
   has_many :jobs, through: :city_jobs
 
   def self.sort_top_cities
-    @cities ||= includes(city_jobs: :job).all.sort_by(&:jobs_count).reject{ |city| city.jobs_count.zero? }.reverse.take(Settings.top.city.limit)
+    @cities ||= all.sort_by(&:jobs_count).reject{ |city| city.jobs_count.zero? }.reverse.take(Settings.top.city.limit)
   end
 
   def self.sort_top_vn_cities
