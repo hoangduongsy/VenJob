e2042cdff5e06910a344797119379dc7f418c8e1
ĐƯờng Sỹ Hoàng <hoangds@zigexn.vn>
2019-12-31 13:58:52 +0700

    User/Register/Show

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,12 @@ Rails.application.routes.draw do
   resources :cities, only: :index
   resources :industries, only: :index
   resource :top, only: :index
+  resource :users, only: :show
   devise_for :users
   as :user do
-    get "login" => "devise/sessions#new", as: :login
+    get "login" , to: "devise/sessions#new"
+    get "registration/", to: "devise/registrations#new"
+    get "my/info", to: "devise/registrations#edit"
+    get "my/:id", to: "users#show"
   end
 end
