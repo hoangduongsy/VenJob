45a665a6e374c0db1a5e5b4a2cb29c40cd9c01a3
ĐƯờng Sỹ Hoàng <hoangds@zigexn.vn>
2019-12-31 15:26:12 +0700

    Fixed sign_in users info

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,8 @@ Rails.application.routes.draw do
   devise_for :users
   as :user do
     get "login" , to: "devise/sessions#new"
-    get "registration/", to: "devise/registrations#new"
+    get "/users/sign_up", to: "devise/registrations#new"
     get "my/info", to: "devise/registrations#edit"
-    get "my/:id", to: "users#show"
+    get "my/", to: "users#show"
   end
 end
