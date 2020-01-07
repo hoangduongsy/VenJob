e45719eccab3b9d4a696dd1d63d6212bdd24b58b
ĐƯờng Sỹ Hoàng <hoangds@zigexn.vn>
2020-01-07 16:29:58 +0700

    Simple format, fixed space

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,7 +1,6 @@
 class Job < ApplicationRecord
   has_many :user_jobs
   has_many :city_jobs
-  belongs_to :city
 
   def self.latest_jobs
     @latest ||= order(created_at: :desc).take(Settings.top.job.limit)
